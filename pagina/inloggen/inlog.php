8be3e963d2747b194d36c13046521ddace063f4b
alwineizema <alwineizema@hotmail.com>
2017-11-29 13:32:40 +0100

    0

--- pagina/inloggen/inlog.php
+++ pagina/inloggen/inlog.php
@@ -1,40 +1,80 @@
-<!doctype html>
-<html>
-    
 <?php
 session_start();
+//print_r( $_SESSION);
 ?>
+
 <!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title>Inlogscherm</title>
-        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" >
-        <link href="./css/style.css" rel="stylesheet" >
+<html lang="en">
+    <head> 
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" href="../../css/bootstrap.css">
+
+        <!-- Website CSS style -->
+        <link rel="stylesheet" type="text/css" href="../../css/login.css">
+
+        <!-- Website Font style -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
+
+        <!-- Fonts -->
+        <link href='https://fonts.googleapis.com/css?family=Passion+One' rel='stylesheet' type='text/css'>
+        <link href='https://fonts.googleapis.com/css?family=Oxygen' rel='stylesheet' type='text/css'>
+
+        <title>Admin</title>
     </head>
     <body>
         <div class="container">
             <?php
             //include die een DIV met HTML print
-            include 'melding.inc.php';
+            include '../inloggen/melding.inc.php';
+            
             
             if (isset($_SESSION['ingelogd']) && $_SESSION['ingelogd']) {
                 //laadt nu pas de beveiligde inhoud
-                include 'geheim.inc.php';
-                print('<a href="verwerk.php?actie=uitloggen">Uitloggen</a>');
+                include '../inloggen/geheim.inc.php';
+                print('<a href="../inloggen/verwerk.php?actie=uitloggen">Uitloggen</a>');
             } else {
                 ?>
+            <div class="row main">
+                <div class="panel-heading">
+                    <div class="panel-title text-center">
+                    </div>
+                </div> 
+                <div class="main-login main-center">
+                    <form class="form-horizontal" method="post" action="../inloggen/verwerk.php">
+                        <div class="form-group">
+                            <label for="username" class="cols-sm-2 control-label">Username</label>
+                            <div class="cols-sm-10">
+                                <div class="input-group">
+                                    <span class="input-group-addon"><i class="fa fa-user fa" aria-hidden="true"></i></span>
+                                    <input type="text" class="form-control" name="gebruikersnaam" id="username"  placeholder="Enter your Username"/>
+                                </div>
+                            </div>
+                        </div>
 
-                <form method="POST" action="verwerk.php" class="form-signin">
-                    <input type="text" name="gebruikersnaam"  class="form-control" placeholder="gebruikersnaam" required autofocus>
-                    <input type="password" name="wachtwoord" class="form-control" placeholder="Wachtwoord" required>
-                    <button type="submit" class="btn btn-lg btn-primary btn-block" >Inloggen</button>
-                </form>
+                        <div class="form-group">
+                            <label for="password" class="cols-sm-2 control-label">Password</label>
+                            <div class="cols-sm-10">
+                                <div class="input-group">
+                                    <span class="input-group-addon"><i class="fa fa-lock fa-lg" aria-hidden="true"></i></span>
+                                    <input type="password" class="form-control" name="wachtwoord" id="password"  placeholder="Enter your Password"/>
+                                </div>
+                            </div>
+                        </div>
 
-                <?php
+                        <div class="form-group ">
+                            <button type="submit" class="btn btn-primary btn-lg btn-block login-button">Sign in</button>
+                        </div>
+                        <div class="login-register">
+                            <a href="create_account.php">Create account</a> or <a href="reset_password.php">reset password</a>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <?php
             }
             ?>
         </div>
+
+        <script type="text/javascript" src="assets/js/bootstrap.js"></script>
     </body>
-</html>
 </html>
