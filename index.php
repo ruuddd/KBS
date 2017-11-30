5e32b1fe3783f91ff67a67efd8998a8fdbe770c8
alwineizema <alwineizema@hotmail.com>
2017-11-30 13:04:37 +0100

    hallo

--- index.php
+++ index.php
@@ -35,8 +35,35 @@
                             <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
                         </div>
                         <div class="col-md-4">
-                            <h2>ARTIKELEN</h2>
-                            <p>test </p>
+                            <div class="slideshow-container">
+                                <div class="mySlides fade">
+                                    <div class="numbertext">1 / 3</div>
+                                    <img src="images/artikelen/sjaal.jpg" height="400" width="65" style="width:100%">
+                                    <div class="text">sjaal</div>
+                                </div>
+
+                                <div class="mySlides fade">
+                                    <div class="numbertext">2 / 3</div>
+                                    <img src="images/artikelen/tas.jpg" height="400" width="65" style="width:100%">
+                                    <div class="text">tas</div>
+                                </div>
+
+                                <div class="mySlides fade">
+                                    <div class="numbertext">3 / 3</div>
+                                    <img src="images/artikelen/jas.jpg" height="400" width="65" style="width:100%">
+                                    <div class="text">jas</div>
+                                </div>
+
+                                <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+                                <a class="next" onclick="plusSlides(1)">&#10095;</a>
+                            </div>
+                            <br>
+
+                            <div style="text-align:center">
+                                <span class="dot" onclick="currentSlide(1)"></span> 
+                                <span class="dot" onclick="currentSlide(2)"></span> 
+                                <span class="dot" onclick="currentSlide(3)"></span> 
+                            </div>
                         </div>
                         <div class="col-md-4">
                             <h2>Heading</h2>
@@ -57,8 +84,9 @@
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script>window.jQuery || document.write('<script src="js/vendor/jquery.min.js"><\/script>')</script>
+    <script>window.jQuery || document.write('<script src="js/vendor/jquery.min.js"><\/script>');</script>
     <script src="js/vendor/popper.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
+    <script src="js/slideshow.js"></script>
 </body>
 </html>
