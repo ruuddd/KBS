9f9983ddec81385ecbfd3573ff8bbf4e7b8e64ae
hotsemeer <32363461+hotsemeer@users.noreply.github.com>
2017-12-05 10:53:18 +0100

    cleanup

--- index.php
+++ index.php
@@ -1,10 +1,6 @@
 <?php
     session_start();
     include 'functions/CRUD/read.php'; 
-
-
-    //print_r( $_SESSION['check']);
-
     include 'functions/dbConnect.php'; 
 ?>
 <!doctype html>
@@ -28,9 +24,6 @@
   </head>
   <body>
 
-    <?php
-    include_once 'pagina/page/header.php';
-    ?>
     <?php include_once 'pagina/page/header.php'; ?>
 
     <main role="main">
@@ -38,46 +31,7 @@
             <div class="jumbotron">
                 <div class="container">
                     <div class="row"> 
-                        <div class="col-md-4">
-                            <h2>Heading</h2>
-                            <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-                        </div>
-                        <div class="col-md-4">
-                            <div class="slideshow-container">
-                                <div class="mySlides fade">
-                                    <div class="numbertext">1 / 3</div>
-                                    <img src="images/artikelen/sjaal.jpg" height="400" width="65" style="width:100%">
-                                    <div class="text">sjaal</div>
-                                </div>
-
-                                <div class="mySlides fade">
-                                    <div class="numbertext">2 / 3</div>
-                                    <img src="images/artikelen/tas.jpg" height="400" width="65" style="width:100%">
-                                    <div class="text">tas</div>
-                                </div>
-
-                                <div class="mySlides fade">
-                                    <div class="numbertext">3 / 3</div>
-                                    <img src="images/artikelen/jas.jpg" height="400" width="65" style="width:100%">
-                                    <div class="text">jas</div>
-                                </div>
-
-                                <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-                                <a class="next" onclick="plusSlides(1)">&#10095;</a>
-                            </div>
-                            <br>
-
-                            <div style="text-align:center">
-                                <span class="dot" onclick="currentSlide(1)"></span> 
-                                <span class="dot" onclick="currentSlide(2)"></span> 
-                                <span class="dot" onclick="currentSlide(3)"></span> 
-                            </div>
-                        </div>
-                        <div class="col-md-4">
-                            <h2>Heading</h2>
-                            <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-                        </div>
-                        <?php print(getPage($_GET['page'], $pdo)["content"]); ?>
+                        <?php print(getPage($_GET['page'], $pdo)); ?>
                     </div>
                 </div>
             </div>
@@ -85,13 +39,6 @@
 
      <?php  include_once 'pagina/page/footer.php'; ?>
 
-        <?php  
-        
-            print_r(getPage($_GET['page'], $pdo));
-            print(getPage($_GET['page'], $pdo)["content"]);
-   
-        include_once 'pagina/page/footer.php';
-        ?>
 
     <!-- Bootstrap core JavaScript
     ================================================== -->
