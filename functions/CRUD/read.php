9042eda3858072da5453b948b2392f6ef6da159e
alwineizema <alwineizema@hotmail.com>
2017-12-07 11:01:53 +0100

    wodeh

--- functions/CRUD/read.php
+++ functions/CRUD/read.php
@@ -12,9 +12,9 @@
             
             function getPage($page, $pdo)
             {                         
-                $stmt = $pdo->prepare("SELECT content FROM content WHERE content_id = '".$page."'");
+                $stmt = $pdo->prepare("SELECT content FROM content WHERE webpage = '".$page."'");
                 $stmt->execute();
                 $arr = $stmt->fetchall(PDO::FETCH_ASSOC);
                 return $arr['0']["content"];
-                $pdo = null;
+                //$pdo = null;
             }
