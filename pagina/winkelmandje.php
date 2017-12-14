eb4265c6ad982da5646f008be328665c43f5c1eb
hotsemeer <32363461+hotsemeer@users.noreply.github.com>
2017-12-14 12:21:04 +0100

    winkelmand display

--- pagina/winkelmandje.php
+++ pagina/winkelmandje.php
@@ -5,6 +5,10 @@
         <link href="../css/winkelmandjenew.css" rel="stylesheet">
     </head>
     <body>
+        <?php
+        checkSessionId();
+        $productInfo = basketProducts($_SESSION['id'], $pdo)
+        ?>
         <div class="container">
             <table id="cart" class="table table-hover table-condensed">
                 <thead>
@@ -17,35 +21,43 @@
                     </tr>
                 </thead>
                 <tbody>
+                    <?php
+                    $totalPrice=0;
+                    foreach ($productInfo as $value)
+                    {
+                        print('
                     <tr>
                         <td data-th="Product">
                             <div class="row">
-                                <div class="col-sm-2 hidden-xs"><img src="http://placehold.it/100x100" alt="..." class="img-responsive"/></div>
+                                <div class="col-sm-2 hidden-xs"><img src="'.$value['product_image'].'" alt="'.$value['product_name'].'" class="img-responsive"/></div>
                                 <div class="col-sm-10">
                                     <h4 class="nomargin">Product 1</h4>
-                                    <p>Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Lorem ipsum dolor sit amet.</p>
+                                    <p>'.$value['product_description'].'</p>
                                 </div>
                             </div>
                         </td>
-                        <td data-th="Price">$1.99</td>
+                        <td data-th="Price">'.$value['product_price'].'</td>
                         <td data-th="Quantity">
-                            <input type="number" class="form-control text-center" value="1">
+                            <input type="number" class="form-control text-center" value="'.$value['amount'].'">
                         </td>
-                        <td data-th="Subtotal" class="text-center">1.99</td>
+                        <td data-th="Subtotal" class="text-center">'.$value['amount']*$value['product_price'].'</td>
                         <td class="actions" data-th="">
                             <button class="btn btn-info btn-sm"><i class="fa fa-refresh"></i></button>
                             <button class="btn btn-danger btn-sm"><i class="fa fa-trash-o"></i></button>								
                         </td>
-                    </tr>
+                    </tr>');
+                                $totalPrice+=$value['product_price']*$value['amount'];
+                    }
+                            ?>
                 </tbody>
                 <tfoot>
                     <tr class="visible-xs">
-                        <td class="text-center"><strong>Total 1.99</strong></td>
+                        <td class="text-center"><strong><?php print($totalPrice); ?></strong></td>
                     </tr>
                     <tr>
                         <td><a href="#" class="btn btn-warning"><i class="fa fa-angle-left"></i> Continue Shopping</a></td>
                         <td colspan="2" class="hidden-xs"></td>
-                        <td class="hidden-xs text-center"><strong>Total $1.99</strong></td>
+                        <td class="hidden-xs text-center"><strong><?php print($totalPrice); ?></strong></td>
                         <td><a href="#" class="btn btn-success btn-block">Checkout <i class="fa fa-angle-right"></i></a></td>
                     </tr>
                 </tfoot>
