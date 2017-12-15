48da42076409f53089f205115da9598256b2520b
alwineizema <alwineizema@hotmail.com>
2017-12-15 10:40:35 +0100

    wqr

--- functions/CRUD/read.php
+++ functions/CRUD/read.php
@@ -1,118 +1,103 @@
-            <?php
-            
-            function check()
-            {
-                $page="home";
-                if(search())
-                {
-                    $page='webshop';
-                }
-                else if (isset($_GET['page']))
-                {
-                    $page = $_GET['page'];
-                }
-                
-                return $page;
-            }
-            
-            function getPage($page, $pdo)
-            {
-                if (checkPage($page, $pdo)) 
-                {
-                    $stmt = $pdo->prepare("SELECT content FROM content WHERE webpage = '".$page."'");
-                    $stmt->execute();
-                    $arr = $stmt->fetchall(PDO::FETCH_ASSOC);
-                    return $arr['0']["content"];
-                    //$pdo = null; 
-                }
-                else
-                {
-                    $stmt = $pdo->prepare("SELECT content FROM content WHERE webpage ='error'");
-                    $stmt->execute();
-                    $arr = $stmt->fetchall(PDO::FETCH_ASSOC);
-                    return $arr['0']["content"];
-                }
-            }
+<?php
 
-            function checkPage($page, $pdo)
-            {
-                $stmt = $pdo->prepare("SELECT webpage FROM content");
-                $stmt->execute();
-                $webpage = $stmt->fetchall(PDO::FETCH_ASSOC);
-                $error = false;
-                foreach ($webpage as $key => $value) {
-                    if ($page == $value["webpage"]) {
-                        $error = true;
-                    }
-                }
-                return $error;
-            }
-            
-            function searchProducts($search, $pdo)
-            {
-                $stmt = $pdo->prepare("SELECT * FROM product p JOIN productcategory PC ON P.product_id = PC.product_id JOIN category C ON C.category_id=PC.category_id WHERE product_name LIKE '%".$search."%' OR category_name LIKE '%".$search."%' OR Product_description LIKE '%".$search."%'");
-                $stmt->execute();
-                $products = $stmt->fetchall(PDO::FETCH_ASSOC);
-                return $products;
-                //$pdo = null;
-            
-            }
-            
-            function search()
-            {
-                if (isset($_POST['search']))
-                {
-                    return true;
-                }
-                else 
-                {
-                    return false;
-                }
-            }
-            
-            function findAllProducts($pdo)
-            {
-                $stmt = $pdo->prepare("SELECT * FROM product"); 
-                $stmt->execute();
-                $allProducts = $stmt->fetchall(PDO::FETCH_ASSOC);
-                return $allProducts;
-            }
-            
-            function findOneProduct($product, $pdo)
-            {
-                $stmt = $pdo->prepare("SELECT * FROM product where product_id = '$product'"); 
-                $stmt->execute();
-                $oneProduct = $stmt->fetchall(PDO::FETCH_ASSOC);
-                return $oneProduct;
-            }
-            
-            function basketProducts($sessionId, $pdo)
-            {
-                $stmt = $pdo->prepare("SELECT * FROM basket LEFT JOIN product ON basket.product_id=product.product_id  JOIN sessie on basket.basket_id=sessie.basket_id  where basket.basket_id = '$sessionId'"); 
-                $stmt->execute();
-                $basketProducts = $stmt->fetchall(PDO::FETCH_ASSOC);
-                return $basketProducts;
-            }
-            
-            function insertSession($pdo)
-            {   
-                $stmt = $pdo->prepare("INSERT INTO sessie (basket_id, order_id) VALUES (,)"); 
-                $stmt->execute();
-                $sessionId=$pdo->lastInsertId();
-                return $sessionId;
-            }
-            
-            function checkSessionId($pdo)
-            {
-                if (!(isset($_SESSION['id'])))
-                {
-                    $_SESSION['id']= insertSession($pdo);
-                }
-                return $_SESSION['id'];
-            }
-            
-            function addProductToBasket($pdo, $product, $sessionId)
-            {
-                $stmt = $pdo->prepare("INSERT INTO basket(basket_id, product_id, amount) VALUES (".$sessionId.",".$product.",1)"); 
-                $stmt->execute();
-            }
+function check() {
+    $page = "home";
+    if (search()) {
+        $page = 'webshop';
+    } else if (isset($_GET['page'])) {
+        $page = $_GET['page'];
+    }
+
+    return $page;
+}
+
+function getPage($page, $pdo) {
+    if (checkPage($page, $pdo)) {
+        $stmt = $pdo->prepare("SELECT content FROM content WHERE webpage = '" . $page . "'");
+        $stmt->execute();
+        $arr = $stmt->fetchall(PDO::FETCH_ASSOC);
+        return $arr['0']["content"];
+        //$pdo = null; 
+    } else {
+        $stmt = $pdo->prepare("SELECT content FROM content WHERE webpage ='error'");
+        $stmt->execute();
+        $arr = $stmt->fetchall(PDO::FETCH_ASSOC);
+        return $arr['0']["content"];
+    }
+}
+
+function checkPage($page, $pdo) {
+    $stmt = $pdo->prepare("SELECT webpage FROM content");
+    $stmt->execute();
+    $webpage = $stmt->fetchall(PDO::FETCH_ASSOC);
+    $error = false;
+    foreach ($webpage as $key => $value) {
+        if ($page == $value["webpage"]) {
+            $error = true;
+        }
+    }
+    return $error;
+}
+
+function searchProducts($search, $pdo) {
+    $stmt = $pdo->prepare("SELECT * FROM product p JOIN productcategory PC ON P.product_id = PC.product_id JOIN category C ON C.category_id=PC.category_id WHERE product_name LIKE '%" . $search . "%' OR category_name LIKE '%" . $search . "%' OR Product_description LIKE '%" . $search . "%'");
+    $stmt->execute();
+    $products = $stmt->fetchall(PDO::FETCH_ASSOC);
+    return $products;
+    //$pdo = null;
+}
+
+function search() {
+    if (isset($_POST['search'])) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function findAllProducts($pdo) {
+    $stmt = $pdo->prepare("SELECT * FROM product");
+    $stmt->execute();
+    $allProducts = $stmt->fetchall(PDO::FETCH_ASSOC);
+    return $allProducts;
+}
+
+function findOneProduct($product, $pdo) {
+    $stmt = $pdo->prepare("SELECT * FROM product where product_id = '$product'");
+    $stmt->execute();
+    $oneProduct = $stmt->fetchall(PDO::FETCH_ASSOC);
+    return $oneProduct;
+}
+
+function basketProducts($sessionId, $pdo) {
+    $stmt = $pdo->prepare("SELECT * FROM basket LEFT JOIN product ON basket.product_id=product.product_id  JOIN sessie on basket.basket_id=sessie.basket_id  where basket.basket_id = '$sessionId'");
+    $stmt->execute();
+    $basketProducts = $stmt->fetchall(PDO::FETCH_ASSOC);
+    return $basketProducts;
+}
+
+function insertSession($pdo) {
+    $stmt = $pdo->prepare("INSERT INTO sessie (basket_id, order_id) VALUES (,)");
+    $stmt->execute();
+    $sessionId = $pdo->lastInsertId();
+    return $sessionId;
+}
+
+function checkSessionId($pdo) {
+    if (!(isset($_SESSION['id']))) {
+        $_SESSION['id'] = insertSession($pdo);
+    }
+    return $_SESSION['id'];
+}
+
+function addProductToBasket($pdo, $product, $sessionId) {
+    $stmt = $pdo->prepare("INSERT INTO basket(basket_id, product_id, amount) VALUES (" . $sessionId . "," . $product . ",1)");
+    $stmt->execute();
+}
+
+function findAllUsers($pdo) {
+    $stmt = $pdo->prepare("SELECT * FROM person");
+    $stmt->execute();
+    $allUsers = $stmt->fetchall(PDO::FETCH_ASSOC);
+    return $allUsers;
+}
--- pagina/phpMail.php
+++ pagina/phpMail.php
@@ -1,18 +1,35 @@
 <?php
 
-$subject = 'Dit is de titel van het test bericht';
-$email = 'Dit is de inhoud van het test bericht';
-$to = 'servicedesk@realhosting.nl';
-$from = 'servicedesk@realhosting.nl';
 
-$headers   = array();
-$headers[] = "MIME-Version: 1.0";
-$headers[] = "Content-type: text/plain; charset=iso-8859-1";
-$headers[] = "From: Realhosting Servicedesk <{$from}>";
-$headers[] = "Reply-To: Realhosting Servicedesk <{$from}>";
-//$headers[] = "Subject: {$subject}";
-$headers[] = "X-Mailer: PHP/".phpversion();
+foreach (findAllUsers($pdo) as $value) {
+    $klantEmail = $value['email'];
+    $klantNaam = $value['firstname'];
+    $klantAchternaam = $value['lastname'];
+}
 
-mail($to, $subject, $email, implode("\r\n", $headers), "-f".$from );
 
-?>
+$from = "<KBS Test> kbsmailtest25@gmail.com";
+$to = "Alwin Eizema <alwineizema@hotmail.com>";
+$subject = "Bedankt voor uw bestelling bij De Ferver!";
+$body = "Beste meneer/mevrouw " . $klantAchternaam. "";
+
+$host = "mail.google.com";
+$username = "kbsmailtest25@gmail.com";
+$password = "Hallo1Hallo1";
+
+$headers = array ('From' => $from,
+  'To' => $to,
+  'Subject' => $subject);
+$smtp = Mail::factory('smtp',
+  array ('host' => $host,
+    'auth' => true,
+    'username' => $username,
+    'password' => $password));
+
+$mail = $smtp->send($to, $headers, $body);
+
+if (PEAR::isError($mail)) {
+  echo("<p>" . $mail->getMessage() . "</p>");
+ } else {
+  echo("<p>Message successfully sent!</p>");
+ }
