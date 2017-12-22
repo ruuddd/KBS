032fc0b7908adb3e83354c31fb41b888798a7afc
alwineizema <alwineizema@hotmail.com>
2017-12-22 13:45:11 +0100

    k

--- home.php
+++ home.php
@@ -7,12 +7,6 @@
 
     <div class="col-md-4">
         <div id="myCarousel" class="carousel slide" data-ride="carousel">
-            <!-- Indicators -->
-            <ol class="carousel-indicators" style="text-align: center">
-                <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-                <li data-target="#myCarousel" data-slide-to="1"></li>
-                <li data-target="#myCarousel" data-slide-to="2"></li>
-            </ol>
 
             <!-- Wrapper for slides -->
             <div class="carousel-inner">
