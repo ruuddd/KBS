e8a7cb443c9ddeaaf1f41fb5f6de602686a64f0b
alwineizema <alwineizema@hotmail.com>
2018-01-04 16:48:22 +0100

    aa

--- pagina/mijn/mijn.php
+++ pagina/mijn/mijn.php
@@ -1,17 +1,5 @@
 <?php
-<<<<<<< HEAD
 print("<div class='col-md-5'><p>Welkom " . $_SESSION['fullname'] . "</p></div>" . "<br>");
-=======
-foreach (findAllUsers($pdo) as $value) {
-    $klantEmail = $value['email'];
-    $klantNaam = $value['firstname'];
-    $klantTussenvoegsel = $value['insertion'];
-    $klantAchternaam = $value['lastname'];
-    //$klantVolledigenaam = $klantNaam . " " . $klantTussenvoegsel . " " . $klantAchternaam;
-}
-
-//print("<div class='col-md-5'><p>Welkom $klantVolledigenaam</p></div>" . "<br>");
->>>>>>> 79437e94ec5f917976030c1e3267d3432c20b29c
 ?>
 
 <div class="col-md-5"><p><a href="/kbs/gegevensAanpassen/">Gegevens aanpassen</a></p></div><br>
