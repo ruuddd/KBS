ddec4c4fd4ccb577c031728b49d9cce2876d549a
hotsemeer <32363461+hotsemeer@users.noreply.github.com>
2018-01-05 19:15:58 +0100

    df

--- pagina/mijn/verwerk.php
+++ pagina/mijn/verwerk.php
@@ -15,10 +15,23 @@ $postcode = filter_input(INPUT_POST, 'zipcode');
 $straatnaam = filter_input(INPUT_POST, 'streetname');
 $huisnummer = filter_input(INPUT_POST, 'addressnumber');
 $email = $_SESSION['emailadres'];
+
 $userCheck = $pdo->prepare("SELECT address_id FROM person WHERE email=?");
 $userCheck->execute($email);
 $addressId = $userCheck->fetch(PDO::FETCH_ASSOC);
 
+    $_SESSION['emailadres'] = $email;
+    $_SESSION['firstname'] = $voornaam;
+    $_SESSION['insertion'] = $tussenvoegsel;
+    $_SESSION['lastname'] = $achternaam;
+    $_SESSION['fullname'] = $_SESSION['firstname'] . " " . $_SESSION['insertion'] . " " . $_SESSION['lastname'];
+    $_SESSION['customer'] = $_SESSION['insertion'] . " " . $_SESSION['lastname'];
+    $_SESSION['phonenumber'] = $telefoonnummer;
+    $_SESSION['country'] = $land;
+    $_SESSION['zipcode'] = $postcode;
+    $_SESSION['streetname'] = $straatnaam;
+    $_SESSION['addressnumber'] = $huisnummer;
+    $_SESSION['city'] = $woonplaats;
 
 if (!empty($voornaam) && !empty($tussenvoegsel) && !empty($achternaam) && !empty($telefoonnummer) && !empty($straatnaam) && !empty($huisnummer) && !empty($woonplaats) && !empty($postcode) && !empty($land)) {
     updateUser($addressId["address_id"], $voornaam, $tussenvoegsel, $achternaam, $telefoonnummer, $land, $woonplaats, $postcode, $straatnaam, $huisnummer, $pdo);
